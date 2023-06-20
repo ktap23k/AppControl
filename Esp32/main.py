c1e4d4e8e4c63d9e2280e35d9522419729274f63
ktap23k <ktap23k@gmail.com>
2023-06-20 20:24:40 +0700

    #add handle barries

--- Esp32/main.py
+++ Esp32/main.py
@@ -1,9 +1,18 @@
 from l298n import *
 import network
-from machine import Pin
+from machine import Pin, PWM
 import time
 from barrier import *
 import _thread
+
+#play sound
+p23 = Pin(23, Pin.OUT)
+buzzer = PWM(p23)
+buzzer.freq(1047)
+
+buzzer.duty(70)
+time.sleep(0.5)
+buzzer.duty(0)
 
 pin = Pin(2, Pin.OUT)
 
@@ -56,18 +65,46 @@ from random import randint
 ws = AsyncWebsocketClient(5)
 
 def barrier(sensor1, sensor2, sensor3, sensor4, ws):
+    def play_sound():
+        buzzer.duty(70)
+        time.sleep(0.3)
+        buzzer.duty(0)
+
     async def send_data(ws):
+        if ws is not None:
+            if await ws.open():
+                await ws.send("ESP32 connect!")
         while True:
             try:
                 sau = sensor1.distance_cm()
+                time.sleep(0.1)
                 phai = sensor2.distance_cm()
+                time.sleep(0.1)
                 truoc = sensor3.distance_cm()
+                time.sleep(0.1)
                 trai = sensor4.distance_cm()
-                time.sleep(0.5)
-                if ws is not None:
-                    if await ws.open():
-                        await ws.send(f"{sau} {phai} {truoc} {trai}")
-                        #print('send: ', distance1, distance2, distance3, distance4)
+                if (sau > 2 and sau < 20) or \
+                   (phai > 2 and phai < 20) or \
+                   (truoc > 2 and truoc < 20) or \
+                   (trai > 2 and trai < 20):
+                        play_sound()
+                        
+                if (sau > 2 and sau < 10) or \
+                   (phai > 2 and phai < 10) or \
+                   (truoc > 2 and truoc < 10) or \
+                   (trai > 2 and trai < 10):
+                        pin.off()
+                        stop()
+                
+                if (sau > 2 and sau < 125) or \
+                   (phai > 2 and phai < 125) or \
+                   (truoc > 2 and truoc < 125) or \
+                   (trai > 2 and trai < 125):
+                        time.sleep(0.5)
+                        if ws is not None:
+                            if await ws.open():
+                                await ws.send(f"{sau} {phai} {truoc} {trai}")
+                                #print('send: ', distance1, distance2, distance3, distance4)
             except Exception as e:
                 print('error: ',e)
         
@@ -82,7 +119,7 @@ async def connect():
         raise Exception('Handshake error.')
     blink_led(pin, 2)
     _thread.start_new_thread(barrier, (sensor1, sensor2, sensor3, sensor4, ws, ))
-    
+
     print('check')
     while True:
         try:
@@ -168,3 +205,4 @@ async def connect():
     print("...handshaked.")
     
 a.run(connect())
+
